ea43ac9e6d3943231340091bfaee839868252e01
Yogesh Bhatt <65986357+ygshbht@users.noreply.github.com>
2024-03-30 05:45:07 +0530

    Open site on new tab automatically when webpack starts

--- webpack.dev.js
+++ webpack.dev.js
@@ -10,48 +10,49 @@ require("dotenv").config({ path: "./.env" });
 //maybe add subsequent .env.local, etc. files based on prcedense
 
 module.exports = {
-	// mode defaults to 'production' if not set
-	mode: mode,
-	entry: "./src/index.ts",
-	// entry not required if using `src/index.js` default
-	// output not required if using `dist/main.js` default
+  // mode defaults to 'production' if not set
+  mode: mode,
+  entry: "./src/index.ts",
+  // entry not required if using `src/index.js` default
+  // output not required if using `dist/main.js` default
 
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: "ts-loader",
-				exclude: /node_modules/,
-			},
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
 
-			{
-				test: /\.glsl$/i,
-				loader: "html-loader",
-			},
-		],
-	},
+      {
+        test: /\.glsl$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
 
-	devtool: "source-map",
-	resolve: {
-		extensions: [".ts", ".js", ".json"],
-	},
+  devtool: "source-map",
+  resolve: {
+    extensions: [".ts", ".js", ".json"],
+  },
 
-	// required if using webpack-dev-server
-	devServer: {
-		// contentBase: "./dist",
-		port: 3000,
-		setupMiddlewares: (middlewares, devServer) => {
-			devServer.app.use(
-				"/",
-				express.static(path.resolve(__dirname, "./public"))
-			);
-			return middlewares;
-		},
-	},
-	plugins: [
-		new HtmlWebpackPlugin({ template: "./template.htm" }),
-		new webpack.DefinePlugin({
-			"process.env": JSON.stringify(process.env),
-		}),
-	],
+  // required if using webpack-dev-server
+  devServer: {
+    // contentBase: "./dist",
+    port: 3000,
+    setupMiddlewares: (middlewares, devServer) => {
+      devServer.app.use(
+        "/",
+        express.static(path.resolve(__dirname, "./public"))
+      );
+      return middlewares;
+    },
+    open: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./template.htm" }),
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify(process.env),
+    }),
+  ],
 };
