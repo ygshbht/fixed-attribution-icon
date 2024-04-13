d976ac1862b40f3d41c141f97315623472d43fb3
Yogesh Bhatt <65986357+ygshbht@users.noreply.github.com>
2024-04-13 21:45:03 +0530

    hande responsivenes

--- src/Attribution.ts
+++ src/Attribution.ts
@@ -7,7 +7,10 @@ export default class FixedAttributionIcon {
   constructor(private attributionLink?: string) {
     this.createDomElement();
     this.addToDom();
+    this.handleResize();
+    this.addResizeListener();
   }
+
   private createDomElement() {
     const container = document.createElement("div");
 
@@ -33,12 +36,12 @@ export default class FixedAttributionIcon {
   }
 
   //TODO: set styles type to object
-  private setElementStyles(styles: any) {
+  private setElementStyles = (styles: any) => {
     for (let key in styles) {
       //   container.style.setProperty(key, styles[key]);
       this.domElement.style[key as any] = styles[key];
     }
-  }
+  };
   setStyles = this.setElementStyles;
 
   //   TODO: make public
@@ -55,10 +58,10 @@ export default class FixedAttributionIcon {
     }
   }
 
-  setSize(width: number, height: number) {
+  setSize = (width: number, height: number) => {
     this.domElement.style.width = `${width}px`;
     this.domElement.style.height = `${height}px`;
-  }
+  };
 
   private createAttributionElement() {
     const link = document.createElement("a");
@@ -68,10 +71,10 @@ export default class FixedAttributionIcon {
     this.attributionLinkElement = link;
   }
 
-  setAttributionLink(attributionLink: string) {
+  setAttributionLink = (attributionLink: string) => {
     this.attributionLinkElement.href = attributionLink;
     // container.appendChild(link);
-  }
+  };
 
   addToDom() {
     document.body.appendChild(this.domElement);
@@ -80,4 +83,31 @@ export default class FixedAttributionIcon {
   removeFromDom() {
     document.body.removeChild(this.domElement);
   }
+
+  removeResizeListener() {
+    window.removeEventListener("resize", this.handleResize);
+  }
+
+  addResizeListener() {
+    window.addEventListener("resize", this.handleResize);
+  }
+  handleResize = () => {
+    let bottomRight = [30, 30];
+    if (innerWidth > 1200) {
+      this.setSize(50, 50);
+    } else if (innerWidth > 900) {
+      this.setSize(40, 40);
+      bottomRight = [25, 25];
+    } else if (innerWidth > 600) {
+      this.setSize(30, 30);
+      bottomRight = [20, 20];
+    } else {
+      this.setSize(25, 25);
+      bottomRight = [15, 15];
+    }
+    this.setElementStyles({
+      bottom: `${bottomRight[0]}px`,
+      right: `${bottomRight[1]}px`,
+    });
+  };
 }
