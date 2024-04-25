24146e879b8e1388434415d45d19f955dfd6238d
Yogesh Bhatt <65986357+ygshbht@users.noreply.github.com>
2024-04-26 02:49:56 +0530

    Comments and arrow functions

--- src/Attribution.ts
+++ src/Attribution.ts
@@ -1,5 +1,11 @@
 import attributionIcon from "./icon";
 
+/** Simply instatiate the class and it will add the icon automatically.
+ *
+ * Usage: new FixedAttributionIcon(url);
+ *
+ * https://github.com/ygshbht/fixed-attribution-icon
+ */
 export default class FixedAttributionIcon {
   domElement!: HTMLDivElement;
   shouldHandleSize = true;
@@ -13,7 +19,7 @@ export default class FixedAttributionIcon {
     this.addResizeListener();
   }
 
-  private createDomElement() {
+  private createDomElement = () => {
     const container = document.createElement("div");
 
     const styles = {
@@ -35,7 +41,7 @@ export default class FixedAttributionIcon {
     this.domElement = container;
     this.domElement.title = "Attribution";
     this.setElementStyles(styles);
-  }
+  };
 
   //TODO: set styles type to object
   private setElementStyles = (styles: any) => {
@@ -47,7 +53,7 @@ export default class FixedAttributionIcon {
   setStyles = this.setElementStyles;
 
   //   TODO: make public
-  private setIcon(icon: HTMLElement, addWrapper = false) {
+  private setIcon = (icon: HTMLElement, addWrapper = false) => {
     this.attributionLinkElement.innerHTML = "";
     if (addWrapper) {
       const container = document.createElement("div");
@@ -58,43 +64,43 @@ export default class FixedAttributionIcon {
     } else {
       this.attributionLinkElement.appendChild(icon);
     }
-  }
+  };
 
   setSize = (width: number, height: number) => {
     this.domElement.style.width = `${width}px`;
     this.domElement.style.height = `${height}px`;
   };
 
-  private createAttributionElement() {
+  private createAttributionElement = () => {
     const link = document.createElement("a");
     if (this.attributionLink) {
       link.href = this.attributionLink;
     }
     this.attributionLinkElement = link;
-  }
+  };
 
   setAttributionLink = (attributionLink: string) => {
     this.attributionLinkElement.href = attributionLink;
     // container.appendChild(link);
   };
 
-  addToDom() {
+  addToDom = () => {
     document.body.appendChild(this.domElement);
-  }
+  };
 
-  removeFromDom() {
+  removeFromDom = () => {
     document.body.removeChild(this.domElement);
-  }
+  };
 
-  removeResizeListener() {
+  removeResizeListener = () => {
     window.removeEventListener("resize", this.handleResize);
-  }
+  };
 
-  addResizeListener() {
+  addResizeListener = () => {
     window.addEventListener("resize", this.handleResize);
-  }
+  };
 
-  handleSize() {
+  handleSize = () => {
     if (innerWidth > 1200) {
       this.setSize(50, 50);
     } else if (innerWidth > 900) {
@@ -104,10 +110,10 @@ export default class FixedAttributionIcon {
     } else {
       this.setSize(25, 25);
     }
-  }
+  };
 
   /** Handle bottom and right fixed position on resize */
-  handlePositioning() {
+  handlePositioning = () => {
     let bottomRight = [30, 30];
     if (innerWidth > 1200) {
     } else if (innerWidth > 900) {
@@ -121,7 +127,7 @@ export default class FixedAttributionIcon {
       bottom: `${bottomRight[0]}px`,
       right: `${bottomRight[1]}px`,
     });
-  }
+  };
 
   handleResize = () => {
     if (this.shouldHandlePositioning) {
